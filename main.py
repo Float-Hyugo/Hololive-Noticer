06bddad7e6396740526c74d87a5875de7ff64ec8
Float-Hyugo <76830994+Float-Hyugo@users.noreply.github.com>
2021-02-03 12:28:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import discord
 from discord.ext import commands
+import check
 itt = discord.Intents.all()
 bot = commands.Bot(prefix="$",intents=itt)
 
-@bot.event()
+@bot.event
 async def on_ready():
   print("ready")
   
+@bot.command()
+@check.is_admin()
+async def load(ctx,module:str):
+  try:
+    bot.load_extension(module)
+  except as e:
+    await ctx.send(f'```\n{e}\n```)
+  else:
   
