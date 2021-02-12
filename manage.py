09dd74b2fc35e12f38526e8e8c06b029ec0722ed
Float-Hyugo <76830994+Float-Hyugo@users.noreply.github.com>
2021-02-12 13:39:56 +0900

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,1 +1,10 @@
+import discord
+from discord.ext import commands
 
+class manage():
+  def __init__(self,bot):
+    self.bot = bot
+    
+  @commands.Cog.listener()
+  async def on_guild_join(self,guild):
+    
